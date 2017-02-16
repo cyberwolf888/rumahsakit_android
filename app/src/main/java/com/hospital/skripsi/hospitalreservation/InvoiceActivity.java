3065034e47c4728cc97a8319a5f663c7d6f8857d
Karen <wijaya.imd@gmail.com>
2017-02-16 19:35:48 +0800

    V 0.7

---
+++ app/src/main/java/com/hospital/skripsi/hospitalreservation/InvoiceActivity.java
@@ -0,0 +1,13 @@
+package com.hospital.skripsi.hospitalreservation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InvoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice);
+    }
+}
--- app/src/main/java/com/hospital/skripsi/hospitalreservation/ReservationActivity.java
+++ app/src/main/java/com/hospital/skripsi/hospitalreservation/ReservationActivity.java
@@ -1,16 +1,23 @@
 package com.hospital.skripsi.hospitalreservation;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -30,9 +37,13 @@ public class ReservationActivity extends AppCompatActivity {
     private DatePickerDialog dp_checkin;
     private EditText etCheckin,etDuration;
     private TextView tvTotal,tvDescription;
+    private Button btnBook;
     private ImageView imageRoom;
     private String id_room;
     private Session session;
+
+    private View mProgressView;
+    private View mFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +57,11 @@ public class ReservationActivity extends AppCompatActivity {
         id_room = getIntent().getStringExtra("id_room");
 
         setContentView(R.layout.activity_reservation);
+
+        mFormView = findViewById(R.id.booking_form);
+        mProgressView = findViewById(R.id.main_progress);
+
+        btnBook = (Button)findViewById(R.id.btnBook);
         etCheckin = (EditText)findViewById(R.id.etCheckin);
         etDuration = (EditText)findViewById(R.id.etDuration);
 
@@ -59,6 +75,13 @@ public class ReservationActivity extends AppCompatActivity {
             public void onClick(View view) {
                 setDateField();
                 dp_checkin.show();
+            }
+        });
+
+        btnBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                booking();
             }
         });
     }
@@ -77,6 +100,7 @@ public class ReservationActivity extends AppCompatActivity {
 
     private void getData(){
         if(isNetworkAvailable()){
+            showProgress(true);
             String url = new RequestServer().getServer_url() + "detailRoom";
             JsonObject jsonReq = new JsonObject();
             jsonReq.addProperty("id_room", id_room);
@@ -97,6 +121,7 @@ public class ReservationActivity extends AppCompatActivity {
                                 public void onCompleted(Exception e, Bitmap result) {
                                     imageRoom.setImageBitmap(result);
                                     imageRoom.getLayoutParams().width = 1280;
+                                    showProgress(false);
                                 }
                             });
                         }
@@ -104,6 +129,71 @@ public class ReservationActivity extends AppCompatActivity {
         }else {
             Toast.makeText(getApplicationContext(), getString(R.string.error_network), Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void booking()
+    {
+        etCheckin.setError(null);
+        etDuration.setError(null);
+
+        String checkin = etCheckin.getText().toString();
+        String duration = etDuration.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(checkin)) {
+            etCheckin.setError(getString(R.string.error_field_required));
+            focusView = etCheckin;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(duration)) {
+            etDuration.setError(getString(R.string.error_field_required));
+            focusView = etDuration;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            if(isNetworkAvailable()){
+                showProgress(true);
+
+                String url = new RequestServer().getServer_url() + "reservation";
+
+                JsonObject jsonReq = new JsonObject();
+                jsonReq.addProperty("user_id", session.getUserId());
+                jsonReq.addProperty("id_room", id_room);
+                jsonReq.addProperty("checkin", checkin);
+                jsonReq.addProperty("duration", duration);
+                Log.d("Data",">"+jsonReq);
+                Ion.with(ReservationActivity.this)
+                        .load(url)
+                        .setJsonObjectBody(jsonReq)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+                                showProgress(false);
+                                String status = result.get("status").toString();
+                                if (status.equals("1")){
+                                    Intent i = new Intent(ReservationActivity.this,InvoiceActivity.class);
+                                    startActivity(i);
+                                    finish();
+                                }else{
+                                    Toast.makeText(ReservationActivity.this,result.get("error").getAsString(),Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+            }else {
+                Toast.makeText(getApplicationContext(), getString(R.string.error_network), Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 
 
@@ -135,6 +225,39 @@ public class ReservationActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
